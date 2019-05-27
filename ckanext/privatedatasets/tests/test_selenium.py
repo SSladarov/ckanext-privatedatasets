7c9f661b9fe2c433c62b8854c1972523c98c8982
ssladarov <ssalvador@conwet.com>
2019-05-27 15:52:03 +0200

    Test Selenium 2

--- ckanext/privatedatasets/tests/test_selenium.py
+++ ckanext/privatedatasets/tests/test_selenium.py
@@ -268,16 +268,12 @@ class TestSelenium(unittest.TestCase):
 
         # Access the dataset
         driver.get(self.base_url + 'dataset/' + dataset_url)
+        if not acquired and private and not in_org:
+            # If the user has not access to the dataset the 404 error page is displayed
+            self.check_ds_values(dataset_url, dataset['private'], dataset['searchable'], dataset['allowed_users'], acquire_url)
 
-        #if not acquired and private and not in_org:
-        #    # If the user has not access to the dataset the 404 error page is displayed
-        #    xpath = '//*[@id="content"]/div[2]/article/div/h1'
-        #    msg = '404 Not Found'
-#
-        #    self.assertEqual(driver.find_element_by_xpath(xpath).text, msg)
-#
-        #else:
-        self.assertEqual(self.base_url + 'dataset/%s' % dataset_url, driver.current_url)
+        else:
+            self.assertEqual(self.base_url + 'dataset/%s' % dataset_url, driver.current_url)
 
     def check_acquired(self, dataset, dataset_url, acquired, private):
         driver = self.driver
